1340e77db7ab20ee4a595898e7744980c4fa4258
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2011-12-22 10:44:35 +0100

    Do not change the current working directory.
    When creating and applying patches, do not switch the current working
    directory of the script to the repository root, but let the repository
    processes operate from the repository root.

--- stash.py
+++ stash.py
@@ -18,20 +18,6 @@ def enum(*sequential, **named):
 
   return type('Enum', (), enums)
 
-def run(command_line, stdin=None, silent=False):
-    """Returns a tuple representing the return code and the resulting output
-    after running the specified *command_line*. All input for
-    the process can be specified using *stdin*. In case *silent*
-    is ``True``, all output of the process is redirected to /dev/null, and
-    ``None`` is returned for the process output.
-    """
-    if silent:
-        fnull = open(os.devnull, 'w')
-        return (subprocess.Popen(command_line, shell=True, stdin=stdin, stdout=fnull, stderr=fnull).wait(), None)
-    else:
-        p = subprocess.Popen(command_line, shell=True, stdin=stdin, stdout=subprocess.PIPE)
-        return (p.wait(), p.communicate()[0].decode('utf-8'))
-
 RepositoryTypes = enum(
   MERCURIAL = 'Mercurial'
   )
@@ -51,24 +37,23 @@ class Stash(object):
 
     PATCHES_PATH = os.path.expanduser('~/.patches')
 
-    def __init__(self):
+    def __init__(self, repository_root):
         # Check if the patches path exists, and in case it does not, create it.
         if not os.path.exists(self.PATCHES_PATH):
             os.mkdir(self.PATCHES_PATH)
 
         self.patches = os.listdir(self.PATCHES_PATH)
 
+        self.repository_root = repository_root
+
     @classmethod
-    def create(cls):
+    def create(cls, path):
         # Determine the repository type, and the root directory for the
         # repository.
-        (repository_root, repository_type) = cls._get_repository_path_and_type()
-
-        # Switch the root process of the current process to the repository root.
-        os.chdir(repository_root)
+        (repository_root, repository_type) = cls._get_repository_path_and_type(path)
 
         if repository_type == RepositoryTypes.MERCURIAL:
-            return MercurialStash()
+            return MercurialStash(repository_root)
 
     @classmethod
     def _get_patch_path(cls, patch_name):
@@ -76,8 +61,9 @@ class Stash(object):
         return os.path.join(cls.PATCHES_PATH, patch_name) if patch_name else None
 
     @classmethod
-    def _get_repository_path_and_type(cls):
-        """Returns a tuple of the root directory and type of the repository.
+    def _get_repository_path_and_type(cls, path):
+        """Returns a tuple of the root directory and type of the repository
+        located at *path*.
 
         :raises: :py:exc:`~StashException` in case no repository was found.
         """
@@ -86,11 +72,10 @@ class Stash(object):
         # Mercurial repository. In case no repository specific folder is found, and
         # the current directory has a parent directory, look if a repository
         # specific directory can be found in the parent directory.
-        current_path = os.getcwd()
-        while current_path != '/':
-            if '.hg' in os.listdir(current_path):
-                return (current_path, RepositoryTypes.MERCURIAL)
-            current_path = os.path.abspath(os.path.join(current_path, os.pardir))
+        while path != '/':
+            if '.hg' in os.listdir(path):
+                return (path, RepositoryTypes.MERCURIAL)
+            path = os.path.abspath(os.path.join(path, os.pardir))
         raise StashException("no valid repository found")
 
     @classmethod
@@ -120,6 +105,20 @@ class Stash(object):
         except:
             raise StashException("patch '%s' does not exist" % patch_name)
 
+    def run(self, command_line, stdin=None, silent=False):
+        """Returns a tuple representing the return code and the resulting output
+        after running the specified *command_line*. All input for
+        the process can be specified using *stdin*. In case *silent*
+        is ``True``, all output of the process is redirected to /dev/null, and
+        ``None`` is returned for the process output.
+        """
+        if silent:
+            fnull = open(os.devnull, 'w')
+            return (subprocess.Popen(command_line, shell=True, stdin=stdin, stdout=fnull, stderr=fnull, cwd=self.repository_root).wait(), None)
+        else:
+            p = subprocess.Popen(command_line, shell=True, stdin=stdin, stdout=subprocess.PIPE, cwd=self.repository_root)
+        return (p.wait(), p.communicate()[0].decode('utf-8'))
+
 class MercurialStash(Stash):
 
     def apply_patch(self, patch_name):
@@ -130,12 +129,12 @@ class MercurialStash(Stash):
         :raises: :py:exc:`~StashException` in case *patch_name* does not exist.
         """
         if patch_name in self.patches:
-            pre_file_status = set(run('hg stat')[1].splitlines())
+            pre_file_status = set(self.run('hg stat')[1].splitlines())
 
             # Apply the patch, and merge with local changes.
-            (patch_returncode, _) = run('patch -p1 --merge', stdin=open(self._get_patch_path(patch_name), 'r'))
+            (patch_returncode, _) = self.run('patch -p1 --merge', stdin=open(self._get_patch_path(patch_name), 'r'))
 
-            post_file_status = set(run('hg stat')[1].splitlines())
+            post_file_status = set(self.run('hg stat')[1].splitlines())
             changed_file_status = post_file_status.difference(pre_file_status)
 
             # Determine all files that have been added.
@@ -153,9 +152,9 @@ class MercurialStash(Stash):
             # Add and remove all files that have been marked as added or removed
             # respectively.
             if added_files:
-                run('hg add %s' % (' '.join(added_files)))
+                self.run('hg add %s' % (' '.join(added_files)))
             if removed_files:
-                run('hg rm %s' % (' '.join(removed_files)))
+                self.run('hg rm %s' % (' '.join(removed_files)))
 
             if patch_returncode == 0:
                 # Applying the patch succeeded, remove stashed patch.
@@ -175,7 +174,7 @@ class MercurialStash(Stash):
         changes in the current repository are reverted.
         """
         # Determine the contents for the new patch.
-        (diff_process_return_code, patch) = run('hg diff -a')
+        (diff_process_return_code, patch) = self.run('hg diff -a')
 
         if diff_process_return_code == 0 and patch:
             # Check if patch already exists, if it does, issue a warning and
@@ -196,13 +195,13 @@ class MercurialStash(Stash):
             patch_file.write(patch.encode('utf-8'))
             patch_file.close()
 
-            pre_file_status = set(run('hg stat')[1].splitlines())
+            pre_file_status = set(self.run('hg stat')[1].splitlines())
 
             if diff_process_return_code == 0:
-                run('hg revert --all', silent=True)
+                self.run('hg revert --all', silent=True)
                 print("Done stashing changes for patch '%s'." % patch_name)
 
-            post_file_status = set(run('hg stat')[1].splitlines())
+            post_file_status = set(self.run('hg stat')[1].splitlines())
             changed_file_status = post_file_status.difference(pre_file_status)
 
             # Remove all files that are created by the patch that is now being
@@ -210,7 +209,7 @@ class MercurialStash(Stash):
             added_files = []
             for status_line in changed_file_status:
                 if status_line[0] == '?':
-                    os.unlink(status_line[2:])
+                    os.unlink(os.path.join(self.repository_root, status_line[2:]))
         else:
             print("No changes to stash, patch '%s' not created." % patch_name)
 
@@ -237,7 +236,7 @@ if __name__ == '__main__':
         elif args.show_patch:
             print Stash.get_patch(args.patch_name)
         elif args.patch_name is not None:
-            stash = Stash.create()
+            stash = Stash.create(os.getcwd())
             if args.apply_patch:
                 stash.apply_patch(args.patch_name)
             else:
